4d704874aaac39b5a2c9692be132451096f46579
wes goodwin <wes.goodwin@gmail.com>
2024-02-26 02:49:51 +0000

    initial commit

--- src/basedub/installs.sh
+++ src/basedub/installs.sh
@@ -9,5 +9,14 @@ apt-get install -y python3.9
 apt-get install -y python3-pip
 apt-get install -y python3-venv
 
+# configure docker in docker
+apt-get install -y docker.io
+usermod -aG docker $USER
 
-#
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+#install go
+apt-get install -y golang-go
